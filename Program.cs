87403f50e06f88ed15760c6dc5bd65c679a5c5ab
Andrew Stakhov <andrew.stakhov@gmail.com>
2021-04-14 23:14:43 -0400

    Initial

---
+++ Program.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Steeltoe.Bootstrap.Autoconfig;
+using System.Linq;
+using System.Threading.Tasks;
+using Steeltoe.Extensions.Configuration.Placeholder;
+
+namespace DemoApp
+{
+    class Program
+    {
+        public static async Task Main()
+        {
+            var logFactory = LoggerFactory.Create(c => c.AddSimpleConsole(c =>
+            {
+                c.SingleLine = true;
+            }));
+            var logger = logFactory.CreateLogger("DemoApp");
+
+            var host = Host.CreateDefaultBuilder()
+                .AddSteeltoe(logFactory)
+                .ConfigureServices(services =>
+            {
+                var svc = services.BuildServiceProvider();
+                var config = (IConfigurationRoot)svc.GetRequiredService<IConfiguration>();
+                logger.LogInformation($"Config providers: ");
+                foreach (var configurationProvider in config.Providers)
+                {
+                    logger.LogInformation($"  - {configurationProvider}");
+                    if(configurationProvider is PlaceholderResolverProvider placeholder)
+                    {
+                        foreach (var subProvider in placeholder.Providers)
+                        {
+                            logger.LogInformation($"    - {subProvider}");
+                        }
+                    }
+                }
+                logger.LogInformation($"DI Container: ");
+                foreach (var registration in services.Where(x => !x.ServiceType.Namespace.StartsWith("Microsoft")).OrderBy(x => x.ServiceType.ToString()))
+                {
+                    logger.LogInformation($"  - {registration.ServiceType}");
+                    var implementations = svc.GetServices(registration.ServiceType);
+                    foreach (var impl in implementations)
+                    {
+                        logger.LogInformation($"    - {impl}");
+                    }
+                }
+            }).Build();
+
+            await Task.Delay(500);
+
+
+
+        }
+    } 
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+# Steeltoe Autoconfiguration Demo
+
+This is a sample app that shows how Steeltoe autoconfiguration works based detected dependencies added to the project. Experiment with adding / removing different steeltoe nuget packages and watch the results of `IConfiguration` and DI Container change with no additional registration calls.
